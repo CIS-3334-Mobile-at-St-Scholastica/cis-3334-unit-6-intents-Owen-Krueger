74e4b9ab815fa5734ac3ef414d47e0a537269f6a
Owen-Krueger <okrueger@css.edu>
2019-02-21 12:45:36 -0600

    Added 3 event starts and calendar events

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -13,6 +13,8 @@ import java.util.GregorianCalendar;
 public class MainActivity extends AppCompatActivity {
 
     Button btn1Web, but1Calendar, btn1Map;
+    Button btn2Web, but2Calendar, btn2Map;
+    Button btn3Web, but3Calendar, btn3Map;
     TextView textViewStatus;
 
     @Override
@@ -48,18 +50,31 @@ public class MainActivity extends AppCompatActivity {
         but1Calendar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
-                textViewStatus.setText("Code should set a calendar event for performance 1");
-                Intent calIntent = new Intent(Intent.ACTION_INSERT);
-                calIntent.setType("vnd.android.cursor.item/event");
-                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
-                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
-                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
-                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
-                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                        begDate.getTimeInMillis());
-                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-                        endDate.getTimeInMillis());
-                startActivity(calIntent);
+                addCalendarEvent(1, "Twin Ports Chorola Project-All Things New" ,
+                        "Mitchell Auditorium", new GregorianCalendar(2019,2,22,19,30),
+                        new GregorianCalendar(2019,2,22,21,30));
+
+            }
+        });
+
+        but2Calendar = (Button) findViewById(R.id.button2Calendar);
+        but2Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                addCalendarEvent(2, "Health, Wholeness, and Humanity: The Stewardship of Creation as if Matter Matters" ,
+                        "Burns Wellness Center 249", new GregorianCalendar(2019,2,26,19,30),
+                        new GregorianCalendar(2019,2,26,20,30));
+
+            }
+        });
+
+        but3Calendar = (Button) findViewById(R.id.button3Calendar);
+        but3Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                addCalendarEvent(3, "The Francis X. Shea Memorial Lecture Presents Poet Jane Hirshfield" ,
+                        "Mitchell Auditorium", new GregorianCalendar(2019,3,5,19,30),
+                        new GregorianCalendar(2019,3,5,20,30));
 
             }
         });
@@ -76,6 +91,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void addCalendarEvent(int perfNum, String title, String location, GregorianCalendar beginDate, GregorianCalendar endingDate){
+        textViewStatus.setText("Code should set a calendar event for performance " + perfNum);
+        Intent calIntent = new Intent(Intent.ACTION_INSERT);
+        calIntent.setType("vnd.android.cursor.item/event");
+        calIntent.putExtra(CalendarContract.Events.TITLE, title);
+        calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, location);
+        GregorianCalendar begDate = beginDate;
+        GregorianCalendar endDate = endingDate;
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                begDate.getTimeInMillis());
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                endDate.getTimeInMillis());
+        startActivity(calIntent);
     }
 
 
